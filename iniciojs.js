7543a5f40fb2247b5ed1fb5981d92ab902fdcc6d
CervandoYR <yactayocervando@gmail.com>
2025-05-01 20:36:58 -0500

    Agregar archivos de página-web (index.html, CSS, JS, static)

--- iniciojs.js
+++ iniciojs.js
@@ -2,3 +2,23 @@
     var myModal = new bootstrap.Modal(document.getElementById('inicioModal'));
     myModal.show();
 });*/
+
+
+
+function mostrarContenido(seccion) {
+ 
+    var secciones = document.querySelectorAll('main div[id]');
+    secciones.forEach(function (sec) {
+        sec.style.display = 'none';
+    });
+
+    var seccionAMostrar = document.getElementById(seccion);
+    if (seccionAMostrar) {
+        seccionAMostrar.style.display = 'block';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    mostrarContenido('inicio'); 
+});
+
